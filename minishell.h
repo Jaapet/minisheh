adb1fb87196b4ed4df0a3df5b084bd46982216d6
ggualerz <ggualerz@student.42nice.fr>
2023-06-10 18:17:42 +0200

    precisions sur le minishell.h

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ggualerz <ggualerz@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:34:22 by ggualerz          #+#    #+#             */
-/*   Updated: 2023/06/10 17:55:05 by ggualerz         ###   ########.fr       */
+/*   Updated: 2023/06/10 18:17:25 by ggualerz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@
 # include <stdbool.h>
 # include <fcntl.h>
 # include "libft/includes/libft.h"
+// TEMPORARY
 # define ERR_DUMMY 1
+// REDIRECTION DEFINES
 # define RD_I_S 1
 # define RD_I_D 2
 # define RD_O_S 3
 # define RD_O_D 4
+// BOOL
 # define TRUE 1
 # define FALSE 0
-
+// MINISHELL SUPER STRUCTURE
 typedef struct s_ms
 {
 	t_node	*node_lst;
@@ -31,7 +34,7 @@ typedef struct s_ms
 	size_t	node_nb;
 	int		*pipes;
 } t_ms;
-
+// NODE OF t_ms.node_lst
 typedef	struct s_node
 {
 	size_t	index;
