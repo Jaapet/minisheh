a9a2a28629b869bf619179a477097f6ddc0bd1aa
ndesprez <ndesprez@student.42nice.fr>
2023-06-12 19:02:09 +0200

    parsing char ** done

--- parsing/line_splitter.c
+++ parsing/line_splitter.c
@@ -6,11 +6,20 @@
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 18:00:11 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/06/11 18:48:57 by ndesprez         ###   ########.fr       */
+/*   Updated: 2023/06/12 18:29:31 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+static char	update_quote(char quote, char cur)
+{
+	if (!quote && (cur == '\'' || cur == '"'))
+		return (cur);
+	else if (quote && cur == quote)
+		return (0);
+	return (quote);
+}
 
 static int	count(char *line)
 {
@@ -23,27 +32,73 @@ static int	count(char *line)
 	quote = 0;
 	while (line[i])
 	{
-		if ((line[i] != '|' || quote) && line[i])
-		{
-			while ((line[i] != '|' || quote) && line[i])
-			{
-				if (!quote && (line[i] == '\'' || line[i] == '"'))
-					quote = line[i];
-				else if (quote && line[i] == quote)
-					quote = 0;
-				i++;
-			}
-			count++;
-		}
+		quote = update_quote(quote, line[i]);
 		if (line[i] == '|' && !quote && line[i])
-			i++;
+			count++;
+		i++;
 	}
-	return (count);
+	return (count + 1);
 }
 
-char	**split_line(char *line);
+static char	*cut_cmd(char *line)
+{
+	int		i;
+	char	*cmd;
+	char	quote;
+
+	i = 0;
+	quote = 0;
+	while ((line[i] != '|' || quote) && line[i])
+	{
+		quote = update_quote(quote, line[i]);
+		i++;
+	}
+	cmd = malloc(sizeof(char) * (i + 1));
+	if (cmd == NULL)
+		return (NULL);
+	i = 0;
+	while ((line[i] != '|' || quote) && line[i])
+	{
+		quote = update_quote(quote, line[i]);
+		cmd[i] = line[i];
+		i++;
+	}
+	cmd[i] = '\0';
+	return (cmd);
+}
+
+char	**split_line(char *line)
+{
+	char	**list;
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = 0;
+	list = ft_calloc((count(line) + 1), sizeof(char *));
+	if (list == NULL)
+		return (NULL);
+	while (*line)
+	{
+		list[i] = cut_cmd(line);
+		i++;
+		while ((*line != '|' || quote) && *line)
+		{
+			quote = update_quote(quote, *line);
+			line++;
+		}
+		if (*line == '|' && !quote && *line)
+			line++;
+	}
+	if (*(line - 1) == '|' && !quote)
+		list[i] = cut_cmd(line - 1);
+	return (list);
+}
 
 // int main(int argc, char **argv)
 // {
 // 	printf("%d\n", count(argv[1]));
+// 	char **list = split_line(argv[1]);
+// 	printf("%s\n%s\n%s\n%s\n%s\n%s\n", list[0], list[1], list[2], list[3], list[4], list[5]);
+// 	printf("c : %c\n", *(argv[1]+1));
 // }
