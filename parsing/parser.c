2115495cd0a8aaf531d12c649a7ad228512ae663
ndesprez <ndesprez@student.42nice.fr>
2023-06-29 21:16:46 +0200

    bullshit

--- parsing/parser.c
+++ parsing/parser.c
@@ -6,72 +6,46 @@
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 17:28:00 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/06/19 20:21:42 by ndesprez         ###   ########.fr       */
+/*   Updated: 2023/06/29 20:53:39 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-static t_node	*create_node(char *cmd)
-{
-	t_node	*new;
-	int		i;
-
-	new = ft_calloc(1, sizeof(t_node));
-	if (!new)
-		return (NULL);
-	new->next = NULL;
-	i = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '<' || cmd[i] == '>')
-			break ;
-		i++;
-	}
-	if (i == ft_strlen(cmd))
-		set_cmd(&new, cmd, 0, i);
-	else
-		proc_redir(&new, cmd);
-	return (new);
-}
-
-static void	add_node(t_node **list, char *cmd)
-{
-	t_node	*new;
-	t_node	*last;
-
-	new = create_node(cmd);
-	if (!new)
-		return ;
-	if (!*list)
-	{
-		*list = new;
-		return ;
-	}
-	last = *list;
-	while (last->next)
-		last = last->next;
-	last->next = new;
-}
-
-static t_node	*proc_cmds(char **cmds)
+static char	**proc(char *word)
 {
 	int		i;
-	t_node	*list;
+	char	quote;
+	char	**list;
+
+	while (word[i])
+	{
+		quote = update_quote(quote, word[i]);
+		if (word[i] == '|' && !quote)
+		{
+			
+		}
+	}
+}
+
+static t_cmd	*proc_cmds(char **cmds)
+{
+	t_cmd	*list;
+	int		i;
 
 	i = 0;
 	while (cmds[i])
 	{
-		add_node(&list, cmds[i]);
+		add_cmd(&list, proc(cmds[i]));
 		i++;
 	}
 	return (list);
 }
 
-t_node	*parse(char *line)
+t_cmd	*parse(char *line)
 {
 	char	**cmds;
-	t_node	*list;
+	t_cmd	*list;
 	//if (!line) error
 	cmds = split_line(line);
 	//if (!cmds) malloc prot
--- parsing/utils.c
+++ parsing/utils.c
@@ -1,21 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.h                                           :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ndesprez <ndesprez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/11 17:27:51 by ndesprez          #+#    #+#             */
-/*   Updated: 2023/06/29 19:15:03 by ndesprez         ###   ########.fr       */
+/*   Created: 2023/06/20 15:13:57 by ndesprez          #+#    #+#             */
+/*   Updated: 2023/06/20 15:13:58 by ndesprez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSER_H
-# define PARSER_H
-
-# include "../minishell.h"
-
-char	update_quote(char quote, char cur);
-char	**split_line(char *line);
-
-#endif
