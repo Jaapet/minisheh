7fdd8aa89030ca828bfb649edd2be83e4cdf7dd1
ggualerz <ggualerz@student.42nice.fr>
2023-07-12 19:19:25 +0200

    yo

--- main.c
+++ main.c
@@ -6,49 +6,25 @@
 /*   By: ggualerz <ggualerz@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 18:36:47 by ggualerz          #+#    #+#             */
-/*   Updated: 2023/07/12 19:14:44 by ggualerz         ###   ########.fr       */
+/*   Updated: 2023/07/12 19:17:34 by ggualerz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
+#include "minishell.h"
 
-// static t_ms *dummy_ms(void)
-// {
-// 	t_ms	*dummy;
+int	main(int ac, char **av, char **env)
+{
+	char	*rl;
 
-// 	dummy = ft_calloc(1, sizeof(t_ms));
-
-// 	return (dummy);
-// }
-
-// int	main(int ac, char **av, char **env)
-// {
-// 	char	*rl;
-
-	g_ms = dummy_ms();
+	g_ms = ft_calloc(1, sizeof(t_ms));
 	ac = 0;
 	av = NULL;
 	g_ms->envp = ft_dup_env(env);
 	ft_banner();
-	// char **cmd;
-	// cmd = ft_calloc(5, sizeof(char *));
-	// cmd[0] = ft_strdup("export");
-	// cmd[1] = ft_strdup("2A=42");
-	// cmd[2] = ft_strdup("B_C=42");
-	// cmd[3] = ft_strdup("C=42");
-	// char **cmd2;
-	// cmd2 = ft_calloc(5, sizeof(char *));
-	// cmd2[0] = ft_strdup("unset");
-	// cmd2[1] = ft_strdup("2A");
-	// cmd2[2] = ft_strdup("C");
-	// // ft_echo(cmd);
-	// // ft_env(ms);
-	// rl = NULL;
 	g_ms->prompt = ft_prompt_str(g_ms->envp);
 	handle();
 	while (1)
 	{
-		// rl = ft_strdup("echo toto | rev > a.txt");
 		rl = readline(g_ms->prompt);
 		if (!rl)
 		{
@@ -70,7 +46,5 @@
 				perror("signal");
 		}
 	}
-	// ft_exec(ms, env);
-	ft_clean_tab(g_ms->envp);
 	return (0);
 }
